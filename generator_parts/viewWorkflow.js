fda295c278f0c8c940f48487d8df0144cf142eb0
wu <daniel.wurzer@bpmspace.org>
2019-08-01 16:57:49 +0200

    smmall change in Workflow View

--- generator_parts/viewWorkflow.js
+++ generator_parts/viewWorkflow.js
@@ -1,16 +1,18 @@
 export default props => {
     
 
-    const T = new Table(props.table);
-    console.log(T);
-    //T.SM.renderHTML('.statemachine');
+    setTimeout(function(){
+        const T = new Table(props.table);
+        T.SM.renderHTML(document.getElementById('statemachine'));
+    }, 1);
 
     return `
     <div>
         <h2>Workflow Diagram</h2>
         <p>of Table <b>${props.table}</b></p>
         <a href="#/${props.table}/read">&larr; Alle Einträge</a>
-
-        <div class="statemachine"></div>
+        <hr>
+        <div id="statemachine"></div>
+        <br>
     </div>`
 }
