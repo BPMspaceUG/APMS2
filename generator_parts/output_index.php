e501c51ede12405dab3a7d9ca99da9c5b8068b97
dwu300 <info@wuda.at>
2020-03-19 20:53:33 +0100

    save access token

--- generator_parts/output_index.php
+++ generator_parts/output_index.php
@@ -1,27 +1,29 @@
 <?php
   require_once(__DIR__."/src/RequestHandler.inc.php");
 
+  $accessToken = null;
+  if (isset($_COOKIE['accesstoken'])) {
+    $accessToken = $_COOKIE['accesstoken'];
+    setcookie('accesstoken', '', time() - 3600); // delete
+  }
 
-  if (isset($_GET['logout'])) {
-    header('Location: ' . URL_LOGOUT);
-    exit();
+  if (isset($_GET['logout'])) { header('Location: '.URL_LOGOUT); exit(); }
+  else if (isset($_GET['emails'])) { header('Location: '.URL_MANAGEPROFILE); exit(); }
+  else if (isset($_GET['changepw'])) { header('Location: '.URL_CHANGEPW); exit(); }
+
+  //-----------------------------------------------
+  if (!isset($_GET['token']) && is_null($accessToken)) {
+    // Login
+    header('Location: ' . getLoginURL());
   }
-  else if (isset($_GET['emails'])) {
-    header('Location: ' . URL_MANAGEPROFILE);
+  else if (is_null($accessToken)) {
+    // Save access token for one request in cookie
+    setcookie('accesstoken', $_GET['token'], time() + 10);
+    header('Location: .');
     exit();
-  }
-  else if (isset($_GET['changepw'])) {
-    header('Location: ' . URL_CHANGEPW);
-    exit();
-  }
-  else {
-    if (!isset($_GET['token'])) {
-      header('Location: ' . getLoginURL());
-    }
   }
   
-
   // Success
   $content = file_get_contents(__DIR__.'/content.inc.html');
-  $content = str_replace('%accessToken%', $_GET['token'], $content);
+  $content = str_replace('%accessToken%', $accessToken, $content);
   echo $content;
